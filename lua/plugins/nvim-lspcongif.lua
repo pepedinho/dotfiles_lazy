eb66f261bf90cc85205c1fbf57659bc90c861dc3
pepedinho <2spii94@gmail.com>
2024-08-10 02:51:20 +0200

    some changes

--- lua/plugins/nvim-lspcongif.lua
+++ lua/plugins/nvim-lspcongif.lua
@@ -1,7 +1,28 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    servers = { eslint = {} },
+    servers = {
+      eslint = {},
+      clangd = { -- Ajouter clangd ici
+        -- Désactiver les modifications de texte supplémentaires
+        init_options = {
+          clangdFileStatus = true, -- Option pour afficher le statut du fichier (peut être modifiée selon vos besoins)
+          completion = {
+            enableAdditionalTextEdit = false, -- Désactiver les modifications de texte supplémentaires
+          },
+        },
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+          "--header-insertion=never",
+        },
+      },
+    },
     setup = {
       eslint = function()
         require("lazyvim.util").lsp.on_attach(function(client)
