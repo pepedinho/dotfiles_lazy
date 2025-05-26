7a3a86220d1335d4a19c1c0ba04c306c6bdab1d3
pepedinho <2spii94@gmail.com>
2025-05-26 12:08:24 +0200

    save

--- init.lua
+++ init.lua
@@ -1,14 +1,2 @@
--- Utiliser des tabulations au lieu d'espaces
-vim.opt.expandtab = false
-
--- Définir la largeur d'une tabulation en termes de colonnes
-vim.opt.tabstop = 4
-
--- Définir la largeur d'une indentation
-vim.opt.shiftwidth = 4
-
--- Utiliser des tabulations pour l'indentation automatique
-vim.opt.smarttab = true
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
